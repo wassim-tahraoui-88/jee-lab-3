4e564320f77f70a4c8ad3031dac56a02b5e87c88
Wassim Tahraoui <wassim.tahraoui@horizon-tech.tn>
2024-11-26 20:32:47 +0100

    - Lab 1: Exercise 1

--- src/main/java/com/horizon/lab_1/controller/FormServlet.java
+++ src/main/java/com/horizon/lab_1/controller/FormServlet.java
@@ -13,8 +13,16 @@ public class FormServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
 		var searchKeywords = req.getParameter("keywords");
-		var searchEngine = Engine.valueOf(req.getParameter("engine"));
-		resp.sendRedirect(searchEngine.getUrl() + searchKeywords);
+
+		var searchEngine = req.getParameter("engine");
+
+		String url = "https://www.google.com/search?q=";
+		if (searchEngine.equals("GOOGLE")) url = "https://www.google.com/search?q=";
+		else if (searchEngine.equals("BING")) url = "https://www.bing.com/search?q=";
+		else if (searchEngine.equals("ASK")) url = "https://fr.ask.com/web?q=";
+
+		resp.sendRedirect(url + searchKeywords);
     }
 }
